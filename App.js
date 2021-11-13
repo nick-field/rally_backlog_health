94787e90d9363014577af7d29c7ed9007a134fbc
Field <nicholas.field@coxautoinc.com>
2021-11-13 16:21:17 -0600

    CUSTOM STORE

--- App.js
+++ App.js
@@ -8,6 +8,42 @@ Ext.define('CustomApp', {
         this._loadData();
     },
 
+    _getRallyBacklogHealthModel: function()
+    {
+        rbhModel = Ext.define('rbh_model', {
+            extend: 'Ext.data.Model',
+            fields: [
+                {name: 'project',     type: 'string'},
+                {name: 'storycount', type: 'int'},
+                {name: 'totalestimate',    type: 'string'}
+
+            ]
+        });
+        
+        return rbhModel;
+    },
+
+    _getRallyBacklogHealthStore: function(groupedData){
+
+
+        for(var i=0; i<groupedData.length; i++){
+            var project = groupedData[i]["name"];
+            console.log(project);
+        }
+
+
+        var rallybhStore = Ext.create('Ext.data.Store', {
+            storeId: "rbh_store",
+            model: "rbh_model",
+            listeners: {
+                scope: this,
+                load: this._loadGrid
+            },
+            data: groupedData
+        });
+        return rallybhStore
+    },
+
 
     _defaultFilters: function(){
         var definedFilter = Ext.create('Rally.data.wsapi.Filter',{
@@ -36,13 +72,13 @@ Ext.define('CustomApp', {
         var defFilters = this._defaultFilters()
         console.log("Default Filters", defFilters.toString())
 
-        var userStoryDataStore = Ext.create(
-            'Rally.data.wsapi.Store', {
+        var userStoryDataStore = Ext.create('Rally.data.wsapi.Store', {
                 model: 'User Story',
                 autoLoad: true,
-                groupField: "Project",
-                groupDir: "ASC",
                 filters: defFilters,
+                groupField: "project",
+                groupDir: 'DESC',
+                pageSize: 1000,
                 listeners: {
                     scope: this,
                     load: this._loadGrid
@@ -58,13 +94,14 @@ Ext.define('CustomApp', {
     
 
     _loadGrid: function(myStore){
-        var myGrid = Ext.create('Rally.ui.grid.Grid', {
-            store: myStore,
-            columnCfgs: ['Project', 'Name', 'ScheduleState', 'PlanEstimate'],
-            features: [{
-                groupHeaderTpl: 'Project: {name}',
-                ftype: 'groupingsummary'
-            }]
+
+        groupedStore = this._getRallyBacklogHealthStore(myStore.getGroups())
+
+        var myGrid = Ext.create('Ext.grid.Panel', {
+            store: Ext.data.StoreManager.lookup('rbh_store'),
+            columns:[
+                {text: 'Project',  dataIndex: 'project' }
+            ]
         });
         this.add(myGrid);
     }
